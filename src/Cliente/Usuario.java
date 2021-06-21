38412178f6a93b58d6069caad151734fe3f2507e
DK-Ghost <dkghost117@gmail.com>
2021-06-21 00:35:37 -0500

    Add files via upload

---
+++ src/Cliente/Usuario.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cliente;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author LAST_
+ */
+public class Usuario implements Serializable{
+    public String nombre;
+    public String apellido;
+    public float dinero;
+    public String nombre_usuario;
+    public int ID;
+    public String contra;
+    
+    public Usuario(int ID, String nombre, String apellido, float dinero, String nombre_usuario,String contrasena) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dinero = dinero;
+        this.nombre_usuario = nombre_usuario;
+        this.ID = ID;
+        this.contra= contrasena;
+    }
+
+    public float getDinero() {
+        return dinero;
+    }
+
+    public void setDinero(float dinero) {
+        this.dinero = dinero;
+    }
+    
+    
+
+    
+    
+}

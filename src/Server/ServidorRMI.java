bbd35673658772c7e03f60bcb1afef8f95f18140
DK-Ghost <dkghost117@gmail.com>
2021-06-21 05:40:24 -0500

    Add files via upload

---
+++ src/Server/ServidorRMI.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Server;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+/**
+ *
+ * @author LAST_
+ */
+public class ServidorRMI {
+
+    public ServidorRMI() {
+        try {
+            ObjetoRemoto obj = new ObjetoRemoto();
+            // hacer registro del obj remoto
+            Registry reg = LocateRegistry.createRegistry(1099);
+            // refencia al objeto remoto
+            reg.rebind("token", obj);
+            System.out.println("servidor activo...");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String args[]) {
+        try {
+            ObjetoRemoto obj = new ObjetoRemoto();
+            // hacer registro del obj remoto
+            Registry reg = LocateRegistry.createRegistry(1099);
+            // refencia al objeto remoto
+            reg.rebind("token", obj);
+            System.out.println("servidor activo...");
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+}
